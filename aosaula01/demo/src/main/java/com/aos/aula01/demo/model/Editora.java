dc4cbce038262dff852d2e336d2f1d002f6962bd
Danilo Santana dos Santos <130468402+DaniloSantos31@users.noreply.github.com>
2024-08-09 10:09:31 -0300

    Add files via upload

---
+++ aosaula01/demo/src/main/java/com/aos/aula01/demo/model/Editora.java
@@ -0,0 +1,23 @@
+package com.aos.aula01.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NonNull;
+
+@Entity
+@Data
+public class Editora {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String nome;
+
+    @NonNull
+    private String CNPJ;
+    
+}
